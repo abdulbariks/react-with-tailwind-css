4061c24610ea15955d7155b3fa5f2c58ed5b4454
Abdul Barik <abdulbarik1997m@gmail.com>
2024-09-23 01:13:19 +0600

    tailwind-css config

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+function App() {
+  return (
+    <div className="container mx-auto px-4 md:container md:mx-auto">
+      <p className=" ml-10 m-5 p-5 border-4">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et nesciunt
+        minus, optio dolores quo, sint omnis repellendus incidunt voluptatem
+        sequi necessitatibus laudantium id beatae, totam quos. Quam dolorum
+        beatae dicta. Veniam a eaque sit laboriosam alias, repellat dolore vitae
+        iure quasi molestias doloremque tempore, at veritatis atque. Iste, ab!
+        Consequatur ad, magni sapiente minima perspiciatis alias nisi sequi
+        rerum assumenda?
+      </p>
+      <div className="ml-10 bg-slate-600 text-center">
+        <p className="text-sm font-medium text-gray-900">Abdul Barik</p>
+        <p className="text-sm text-gray-500">abdulbarik1997m@gmail.com</p>
+      </div>
+
+      <div className="ml-10 columns-2 box-border  p-4 border-4 ">
+        <img
+          className="box-border h-32 w-32 p-4 border-4 "
+          src="https://unsplash.com/photos/a-pink-flower-with-a-white-background-Y9JOITOAZQw"
+          alt="pink flower"
+        />
+        <img
+          className="box-border h-32 w-32 p-4 border-4"
+          src="https://unsplash.com/photos/a-pink-flower-with-a-white-background-Y9JOITOAZQw"
+          alt="pink flower"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
